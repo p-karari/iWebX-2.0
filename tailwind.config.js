45d2bbbe390fe2cf77ee5ee1fdd6efe9b9f2aecb
p-karari <kararikelvin2002@gmail.com>
2025-09-08 17:10:49 +0300

    Config files

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     // ... your content paths
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         // Iwebx Brand Colors
+//         gold: {
+//           500: '#D4AF37', // Primary gold
+//         },
+//         navy: {
+//           900: '#0A192F', // Dark navy blue
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
