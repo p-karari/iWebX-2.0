a9de617bfbb0461dd4930899b076778b29d3c288
p-karari <kararikelvin2002@gmail.com>
2026-02-07 16:34:18 +0300

    face lift/design updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,17 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 
+// import { Metadata } from 'next';
+
+// This object tells Next.js exactly what to put in the <head>
 export const metadata: Metadata = {
-  title: "iWebX - Innovative Web Solutions",
-  description: "iWebX Technologies",
-};
+  title: 'Iwebx digital solutions | home page', 
+  verification: {
+    other: {
+      'trustpilot-one-time-domain-verification-id': '00ce61c7-ccfd-49f0-b786-cf23f1849bd4',
+    },
+  },
+}
 
 export default function RootLayout({
   children,
